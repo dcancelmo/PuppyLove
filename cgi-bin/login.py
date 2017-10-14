7d84e6f1ff05d34cae77e07cc638cde5b0738135
Kayla Sell <ksell@u.rochester.edu>
2017-10-14 19:15:55 -0400

    fixed so no server error

--- cgi-bin/login.py
+++ cgi-bin/login.py
@@ -1,4 +1,4 @@
-#!C:\Python.exe
+#!/usr/bin/env python
 
 import cgitb
 import cgi
@@ -7,46 +7,50 @@ from hashlib import sha256
 
 cgitb.enable()
 
-print "Content-Type: text/html"
-print "Set-Cookie: userName=value"
-print ""
+print 'Content-Type: text/html'
+print
 
 form = cgi.FieldStorage()
 
-userName = form["name"].value
-password = form["password"].value
+userName = form['username'].value
+password = form['password'].value
 
 conn = sqlite3.connect('createUser.db')
 c = conn.cursor()
-
-c.execute('CREATE TABLE IF NOT EXISTS users(username varchar(30) primary key, password varchar(30))')
+    
+c.execute('CREATE TABLE IF NOT EXISTS users(username varchar(30) primary key, password char(64), timeCreated varchar(26))')
 
 rows = c.execute('SELECT * FROM users WHERE username = ?', [userName])
+rows = rows.fetchone();
 
-for row in rows:
-	hashed_pass = row['password']
-	salt = row['timeCreated']
-	test_pass = password + salt
-	test_pass = sha256(test_pass.encode('ascii')).hexdigest()
-	if test_pass == hashed_pass:
-		# DO SOMETHING HERE
-	else:
-		#do something like this
-		raise Exception('Incorrect username/password')
+hashed_pass = rows[1]
+salt = rows[2]
+test_pass = password + salt
+test_pass = sha256(test_pass.encode('ascii')).hexdigest();
+if hashed_pass == test_pass:
+    print '''<html>
+        <head>
+            <title>Correct Login</title>
+        </head>
+        <body>
+        <p>'''
+    print "Your name: " + userName
 
+    print "Your Password: " + password
+    print '''</p>
+        </body>
+        </html>
+        '''
+else:
+    print '''<html>
+        <head>
+            <title>Incorrect Login</title>
+        </head>
+        <body>
+            <p>Incorrect username/password</p>
+        </body>
+        </html>
+        '''
 
 conn.commit()
 conn.close()
-
-print '''<html>
-    <head>
-    </head>
-    <body>
-        <p>'''
-print "Your name: " + name
-
-print "Your Password: " + password
-'''</p>
-    </body>
-</html>
-'''
