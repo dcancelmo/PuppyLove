6743e4c11f7a98d02d14f941193410209588dd53
Kayla Sell <ksell@u.rochester.edu>
2017-12-07 14:15:21 -0500

    saves and loads messages

---
+++ cgi-bin/load-messages.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import cgitb
+import os
+import cgi
+import sqlite3
+import json
+
+cgitb.enable()
+
+conn = sqlite3.connect('createUser.db')
+conn.text_factory = str #need to change the text_factory for the images
+c = conn.cursor()
+
+messages_rows = c.execute("SELECT * FROM messages").fetchall()
+
+data = []
+
+if messages_rows is not None:
+	for row in messages_rows:
+		entry = {
+			'message': row[0],
+			'username': row[1]
+		}
+		data.append(entry)
+	print
+	print json.dumps(data)
---
+++ cgi-bin/store-message.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import cgitb
+import os
+import cgi
+import sqlite3
+import json
+
+print 
+
+cgitb.enable()
+
+conn = sqlite3.connect('createUser.db')
+conn.text_factory = str #need to change the text_factory for the images
+c = conn.cursor()
+
+parameters = cgi.FieldStorage()
+message = parameters.getvalue("message");
+username = parameters.getvalue("username");
+
+c.execute('CREATE TABLE IF NOT EXISTS messages(message varchar(500), username varchar(30))')
+c.execute('INSERT INTO messages(message, username) VALUES(?, ?)',[message, username])
+
+conn.commit()
+conn.close()
+
+print
